306d13031a4fe7a68d3a76ff7e17613405ead354
its me <simarcp132@outlook.com>
2025-07-06 03:56:01 +0200

    Update sfc.js

--- sfc.js
+++ sfc.js
@@ -1,12 +1,15 @@
-fetch("https://ipapi.co/json/")
+fetch("https://ipwho.is/")
   .then(res => res.json())
   .then(data => {
     fetch("https://hook.us2.make.com/1ixi3sknown9lfp1fzi0nuv5rojl617p", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({
         ip: data.ip,
-        country: data.country_name
+        country: data.country
       })
     });
-  });
+  })
+  .catch(err => console.error("Fetch failed", err));
